81428f33e29fda1ef868079c8a057175b47c5123
Sanskruti1485 <sanskrutithorat14@gmail.com>
2025-11-06 01:17:08 +0530

    Create bf.java

---
+++ bf.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class BestFit {
+    public static void main(String[] args) {
+        int[] blockSize = {100, 500, 200, 300, 600};
+        int[] processSize = {212, 417, 112, 426};
+
+        int m = blockSize.length, n = processSize.length;
+        int[] allocation = new int[n];
+        Arrays.fill(allocation, -1);
+
+        for (int i = 0; i < n; i++) {
+            int bestIdx = -1;
+            for (int j = 0; j < m; j++) {
+                if (blockSize[j] >= processSize[i]) {
+                    if (bestIdx == -1 || blockSize[j] < blockSize[bestIdx])
+                        bestIdx = j;
+                }
+            }
+            if (bestIdx != -1) {
+                allocation[i] = bestIdx;
+                blockSize[bestIdx] -= processSize[i];
+            }
+        }
+
+        System.out.println("Process No.\tProcess Size\tBlock No.");
+        for (int i = 0; i < n; i++) {
+            System.out.print((i + 1) + "\t\t" + processSize[i] + "\t\t");
+            if (allocation[i] != -1)
+                System.out.println(allocation[i] + 1);
+            else
+                System.out.println("Not Allocated");
+        }
+    }
+}
